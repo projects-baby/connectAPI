0bef6fb2e52f03f07d0af4c5d995904c95ec5cfe
Olajide Amoran <workstation@Olajides-MacBook-Pro.local>
2019-08-28 10:06:49 -0400

    javascriptupdated

--- scripts.js
+++ scripts.js
@@ -1,17 +1,21 @@
+// create request varabile and assign XMLHttpRequest object to it
 var request = new XMLHttpRequest()
 
+// open a new connectiong, using the GET request on the URL endpoint
 request.open('GET', 'https://ghibliapi.herokuapp.com/films', true)
-request.onload = function() {
-  // Begin accessing JSON data here
-  var data = JSON.parse(this.response)
 
-  if (request.status >= 200 && request.status < 400) {
+request.onload = function () {
+// begin accessing JSON data below
+var data = JSON.parse(this.response)
+
+if (request.status >= 200 && request.status < 400) {
     data.forEach(movie => {
-      console.log(movie.title)
-    })
-  } else {
+    // log each movie title
+    console.log(movie.title)
+})
+} else {
     console.log('error')
-  }
 }
 
+// send request
 request.send()
